fd586d6cdfd3bea331579ad37fe8cbdc983362a6
GustavoVictorio <gu.victorio3@gmail.com>
2021-09-07 11:39:32 -0300

    Project Created

---
+++ backend/src/main/java/com/GustavoVictorio/VendasDS/VendasDsApplication.java
@@ -0,0 +1,13 @@
+package com.GustavoVictorio.VendasDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasDsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/GustavoVictorio/VendasDS/VendasDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GustavoVictorio.VendasDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
