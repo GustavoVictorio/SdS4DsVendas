ed9bdbc5427c1ab979ed3960409a55c8509849c5
GustavoVictorio <gu.victorio3@gmail.com>
2021-09-08 22:10:20 -0300

    Domain model, database seed

--- backend/src/main/java/com/GustavoVictorio/VendasDS/entities/Sale.java
+++ backend/src/main/java/com/GustavoVictorio/VendasDS/entities/Sale.java
@@ -3,15 +3,29 @@ package com.GustavoVictorio.VendasDS.entities;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
 public class Sale  implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idLong;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
 	private Seller seller;
 
 	public Sale() {
--- backend/src/main/java/com/GustavoVictorio/VendasDS/entities/Seller.java
+++ backend/src/main/java/com/GustavoVictorio/VendasDS/entities/Seller.java
@@ -4,12 +4,24 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
 public class Seller implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
+	@OneToMany(mappedBy = "seller")
 	private List<Sale> sales = new ArrayList<>();
 
 	public Seller() {
