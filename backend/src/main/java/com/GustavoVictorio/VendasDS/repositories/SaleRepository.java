3fc957a0727d70b50b78ce637bf850f6c6f89ec9
GustavoVictorio <gu.victorio3@gmail.com>
2021-09-10 09:29:46 -0300

    Group by search

--- backend/src/main/java/com/GustavoVictorio/VendasDS/repositories/SaleRepository.java
+++ backend/src/main/java/com/GustavoVictorio/VendasDS/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.GustavoVictorio.VendasDS.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.GustavoVictorio.VendasDS.dto.SaleSuccessDTO;
+import com.GustavoVictorio.VendasDS.dto.SaleSumDTO;
 import com.GustavoVictorio.VendasDS.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 	
+	@Query("SELECT new com.GustavoVictorio.VendasDS.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.GustavoVictorio.VendasDS.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/GustavoVictorio/VendasDS/service/SaleService.java
+++ backend/src/main/java/com/GustavoVictorio/VendasDS/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.GustavoVictorio.VendasDS.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.GustavoVictorio.VendasDS.dto.SaleDTO;
+import com.GustavoVictorio.VendasDS.dto.SaleSuccessDTO;
+import com.GustavoVictorio.VendasDS.dto.SaleSumDTO;
 import com.GustavoVictorio.VendasDS.entities.Sale;
 import com.GustavoVictorio.VendasDS.repositories.SaleRepository;
 import com.GustavoVictorio.VendasDS.repositories.SellerRepository;
@@ -25,6 +29,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 
 }
